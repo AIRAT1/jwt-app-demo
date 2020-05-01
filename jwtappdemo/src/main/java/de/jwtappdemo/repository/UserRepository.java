a7446c6513a1c5fc8ddc2bf9e13828c5893c513a
Ayrat Hairullin <ayrat1@mail.ru>
2020-05-02 00:46:46 +0300

    33.00

---
+++ jwtappdemo/src/main/java/de/jwtappdemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package de.jwtappdemo.repository;
+
+import de.jwtappdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String name);
+}
